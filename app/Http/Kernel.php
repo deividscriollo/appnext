fc8204f2eb1588ab3ef5f239cf85fe3fcd3e7b96
Ald121 <fallsouls@hotmail.com>
2016-07-13 16:56:19 -0500

    existencia de usuario nextbook

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,6 +51,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'jwt.auth' => 'Tymon\JWTAuth\Middleware\GetUserFromToken',
         'jwt.refresh' => 'Tymon\JWTAuth\Middleware\RefreshToken',
+        'cors' => \App\Http\Middleware\CORS::class,
         // 'login-middleware' => \App\Http\Middleware\loginMiddleware::class,
     ];
 }
