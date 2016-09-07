0bc9918b8a90cd037b0df0687004617d5aee0759
Ald121 <fallsouls@hotmail.com>
2016-09-07 17:51:34 -0500

    cambios

--- config/global.php
+++ config/global.php
@@ -1,7 +1,8 @@
 <?php 
 return [
 
-	'appnext' => 'http://192.168.0.106/appnext'
+	'appnext' => 'http://192.168.0.106/appnext',
+	'appserviciosnext' => 'http://192.168.0.106/appserviciosnext'
 	// 'appnext' => 'https://servicios.nextbook.ec'
 
 ];
