ebb76b9b68e446880da5b7277a038873bf135c5c
Alex <fallsouls@hotmail.com>
2016-09-20 16:41:55 -0500

    cambio de ip

--- config/global.php
+++ config/global.php
@@ -1,8 +1,8 @@
 <?php 
 return [
 
-	'appnext' => 'http://192.168.0.104/appnext',
-	'appserviciosnext' => 'http://192.168.0.104/appserviciosnext'
+	'appnext' => 'http://186.33.168.251/appnext',
+	'appserviciosnext' => 'http://186.33.168.251/appserviciosnext'
 	// 'appnext' => 'https://servicios.nextbook.ec'
 
 ];
